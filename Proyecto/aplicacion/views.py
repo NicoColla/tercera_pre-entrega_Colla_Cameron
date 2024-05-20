caf88ba540e5db7f9dbf37ca90a808a1d6e8191f
Nicolas Colla Cameron <ns.colla@gmail.com>
2024-05-20 00:42:11 -0300

    Correccion

--- Proyecto/aplicacion/views.py
+++ Proyecto/aplicacion/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .forms import ProductoForm, MarcaForm, StockForm
 from .models import Producto, MarcaProducto, Stock
 
 def index(request):
     return render(request, 'aplicacion/index.html')
 
-def buscar_producto(request):
-    return render(request, 'buscar_producto.html')
+def buscar(request):
+    productos = Producto.objects.all()
+    marcas = MarcaProducto.objects.all()
+    stocks = Stock.objects.all()
+    context = {
+        'productos': productos,
+        'marcas': marcas,
+        'stocks': stocks
+    }
+    return render(request, 'aplicacion/buscar.html', context)
 
 def agregar_marca(request):
     if request.method == 'POST':
@@ -16,14 +25,15 @@ def agregar_marca(request):
             nombre = form.cleaned_data['marca']
             precio = form.cleaned_data['precio']
 
-            marca_obj, _ = MarcaProducto.objects.get_or_create(nombre=nombre, tipo_producto=producto, precio=precio)
-            marca_obj.save()
-
+            marca_obj, created = MarcaProducto.objects.get_or_create(nombre=nombre, tipo_producto=producto, precio=precio)
+            if created:
+                messages.success(request, 'Marca agregada exitosamente.')
+            else:
+                messages.info(request, 'La marca ya existe.')
             return redirect('index')
     else:
         form = MarcaForm()
     return render(request, 'agregar/agregar_marca.html', {'form': form})
-
 
 def agregar_stock(request):
     if request.method == 'POST':
@@ -33,13 +43,9 @@ def agregar_stock(request):
             cantidad = form.cleaned_data['cantidad']
             stock_obj, created = Stock.objects.get_or_create(marca=marca)
 
-            if not created:
-                stock_obj.cantidad += cantidad
-                stock_obj.save()
-            else:
-                stock_obj.cantidad = cantidad
-                stock_obj.save()
-
+            stock_obj.cantidad += cantidad
+            stock_obj.save()
+            messages.success(request, 'Stock agregado exitosamente.')
             return redirect('index')
     else:
         form = StockForm()
@@ -51,6 +57,7 @@ def agregar_producto(request):
         if form.is_valid():
             nombre_producto = form.cleaned_data['producto']
             Producto.objects.get_or_create(nombre=nombre_producto)
+            messages.success(request, 'Producto agregado exitosamente.')
             return redirect('index')
     else:
         form = ProductoForm()
@@ -62,6 +69,7 @@ def eliminar_producto(request):
         producto_id = request.POST.get('producto_id')
         producto = get_object_or_404(Producto, pk=producto_id)
         producto.delete()
+        messages.success(request, 'Producto eliminado exitosamente.')
         return redirect('index')
     return render(request, 'eliminar/eliminar_producto.html', {'productos': productos})
 
@@ -71,6 +79,7 @@ def eliminar_marca(request):
         marca_id = request.POST.get('marca_id')
         marca = get_object_or_404(MarcaProducto, pk=marca_id)
         marca.delete()
+        messages.success(request, 'Marca eliminada exitosamente.')
         return redirect('index')
     return render(request, 'eliminar/eliminar_marca.html', {'marcas': marcas})
 
@@ -80,13 +89,18 @@ def eliminar_stock(request):
         stock_id = request.POST.get('stock_id')
         stock = get_object_or_404(Stock, pk=stock_id)
         cantidad_a_eliminar = int(request.POST.get('cantidad', 0))
-        
-        if cantidad_a_eliminar > 0 and cantidad_a_eliminar <= stock.cantidad:
-            stock.cantidad -= cantidad_a_eliminar
-            stock.save()
-            return redirect('index')
-        else:
-            error_message = "La cantidad ingresada es inválida o excede la cantidad disponible en el stock."
-            return render(request, 'eliminar/eliminar_stock.html', {'stocks': stocks, 'error_message': error_message})
 
+        if cantidad_a_eliminar <= 0 or cantidad_a_eliminar > stock.cantidad:
+            messages.error(request, 'Cantidad inválida.')
+            return render(request, 'eliminar/eliminar_stock.html', {'stocks': stocks})
+
+        stock.cantidad -= cantidad_a_eliminar
+        if stock.cantidad == 0:
+            stock.delete()
+        else:
+            stock.save()
+
+        messages.success(request, 'Stock eliminado exitosamente.')
+        return redirect('index')
     return render(request, 'eliminar/eliminar_stock.html', {'stocks': stocks})
+
--- Proyecto/configuracion/urls.py
+++ Proyecto/configuracion/urls.py
@@ -4,7 +4,7 @@ from aplicacion import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('buscar-producto/', views.buscar_producto, name='buscar_producto'),
+    path('buscar/', views.buscar, name='buscar'),  # Nota: Cambié a 'buscar' en lugar de 'buscar-producto'
     path('agregar-producto/', views.agregar_producto, name='agregar_producto'),
     path('agregar-marca/', views.agregar_marca, name='agregar_marca'),
     path('agregar-stock/', views.agregar_stock, name='agregar_stock'),
